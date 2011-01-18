46389662225972938fbe14d7d60f6ffe1d564748
Nick Berardi <nick@managedfusion.com>
2011-01-18 08:24:21 -0500

    merged CoderJournal rules in to the tests of ManagedFusion.Rewriter as a good reference for creating plugins

---
+++ test/CoderJournal.Rewriter.Rules/PostQueryStringRuleAction.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data;
+using ManagedFusion.Rewriter;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+namespace CoderJournal.Rewriter.Rules
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class PostQueryStringRuleAction : IRuleAction
+	{
+		#region IRuleAction Members
+
+		/// <summary>
+		/// Gets the pattern.
+		/// </summary>
+		/// <value>The pattern.</value>
+		public Pattern Pattern
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Gets the text.
+		/// </summary>
+		/// <value>The text.</value>
+		public string Substitution
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Processes the specified log level.
+		/// </summary>
+		/// <param name="context">The context.</param>
+		/// <returns></returns>
+		public void Execute(RuleContext context)
+		{
+			string inputUrl = context.CurrentUrl.GetComponents(UriComponents.PathAndQuery, UriFormat.UriEscaped);
+			string sqlCommand = Pattern.Replace(inputUrl, Substitution, context);
+			string substituedUrl;
+
+			using (MySqlConnection connection = new MySqlConnection(Properties.Settings.Default.DatabaseConnection))
+			using (MySqlCommand command = connection.CreateCommand())
+			{
+				command.CommandText = sqlCommand;
+				command.CommandType = CommandType.Text;
+
+				try
+				{
+					connection.Open();
+					substituedUrl = command.ExecuteScalar() as string;
+				}
+				finally
+				{
+					connection.Close();
+				}
+			}
+
+			context.SubstitutedUrl = new Uri(context.CurrentUrl, substituedUrl);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="context"></param>
+		/// <returns></returns>
+		public virtual bool IsMatch(RuleContext context)
+		{
+			string testUrl = context.CurrentUrl.AbsolutePath;
+
+			return Pattern.IsMatch(testUrl);
+		}
+
+		/// <summary>
+		/// Inits the specified text.
+		/// </summary>
+		/// <param name="pattern">The pattern.</param>
+		/// <param name="substitution">The text.</param>
+		public void Init(Pattern pattern, string substitution)
+		{
+			Pattern = pattern;
+			Substitution = substitution;
+		}
+
+		#endregion
+	}
+}
---
+++ test/CoderJournal.Rewriter.Rules/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CoderJournal.Rewriter.Rules")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Managed Fusion")]
+[assembly: AssemblyProduct("CoderJournal.Rewriter.Rules")]
+[assembly: AssemblyCopyright("Copyright © Managed Fusion 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b26bdd0-101c-4525-b83d-cb17c1e289ed")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
