e69deefe81cf1fc14bc6c4e6c733ca8015033916
Nick Berardi <nick@managedfusion.com>
2013-06-24 10:32:39 -0400

    updated build file

--- build.cmd
+++ build.cmd
@@ -4,8 +4,8 @@ if "%config%" == "" (
    set config=Debug
 )
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild .\src\FluentCassandra.csproj /p:Configuration="%config%" /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild .\src\ManagedFusion.Rewriter.csproj /p:Configuration="%config%" /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 if "%config%" == "Release" (
-	nuget pack .\src\FluentCassandra.csproj -Build -Symbols -Properties Configuration=Release
+	nuget pack .\src\ManagedFusion.Rewriter.csproj -Build -Symbols -Properties Configuration=Release
 )
